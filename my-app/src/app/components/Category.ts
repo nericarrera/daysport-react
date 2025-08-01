de66cfa128e7e162d5ec756eee7f0ca24342c6c7
nericarrera <nericarrera@yahoo.com.ar>
2025-08-01 10:13:50 -0300

    ok

---
+++ my-app/src/app/components/Category.ts
@@ -0,0 +1,47 @@
+export const categories = [
+  {
+    name: "MUJER",
+    href: "/mujer",
+    subcategories: [
+      { name: "Remeras", href: "/mujer/remeras", image: "/img/categories/mujer-remeras.jpg" },
+      { name: "Buzos", href: "/mujer/buzos", image: "/img/categories/mujer-buzos.jpg" },
+      { name: "Joggins", href: "/mujer/joggins", image: "/img/categories/mujer-joggins.jpg" },
+      { name: "Camperas", href: "/mujer/camperas", image: "/img/categories/mujer-camperas.jpg" },
+      { name: "Calzas", href: "/mujer/calzas", image: "/img/categories/mujer-calzas.jpg" },
+      { name: "Zapatillas", href: "/mujer/zapatillas" }
+    ]
+  },
+  {
+    name: "HOMBRE",
+    href: "/hombre",
+    subcategories: [
+      { name: "Remeras", href: "/hombre/remeras", image: "/img/categories/hombre-remeras.jpg" },
+      { name: "Buzos", href: "/hombre/buzos", image: "/img/categories/hombre-buzos.jpg" },
+      { name: "Pantalones", href: "/hombre/pantalones", image: "/img/categories/hombre-pantalones.jpg" },
+      { name: "Camperas", href: "/hombre/camperas", image: "/img/categories/hombre-camperas.jpg" },
+      { name: "Shorts", href: "/hombre/shorts", image: "/img/categories/hombre-shorts.jpg" }
+    ]
+  },
+    {
+        name: "NIÑOS",
+        href: "/ninos",
+        subcategories: [
+        { name: "Remeras", href: "/ninos/remeras", image: "/img/categories/ninos-remeras.jpg" },
+        { name: "Buzos", href: "/ninos/buzos", image: "/img/categories/ninos-buzos.jpg" },
+        { name: "Pantalones", href: "/ninos/pantalones", image: "/img/categories/ninos-pantalones.jpg" },
+        { name: "Camperas", href: "/ninos/camperas", image: "/img/categories/ninos-camperas.jpg" },
+        { name: "Zapatillas", href: "/ninos/zapatillas" }
+        ]
+    },
+    {
+         name: "ACCESORIOS", 
+      href: "/accesorios", 
+      subcategories: [
+        { name: "Gorras", href: "/accesorios/gorras", image: "/img/categories/accesorios-gorras.jpg" },
+        { name: "Bolsos", href: "/accesorios/bolsos", image: "/img/categories/accesorios-bolsos.jpg" },
+        { name: "Calcetines", href: "/accesorios/calcetines", image: "/img/categories/accesorios-calcetines.jpg" },
+        { name: "Cinturones", href: "/accesorios/cinturones", image: "/img/categories/accesorios-cinturones.jpg" },
+        { name: "Gafas de sol", href: "/accesorios/gafas-de-sol", image: "/img/categories/accesorios-gafas-de-sol.jpg" }
+      ]
+    },
+];
