69d2596e1179511766bb5652515810be160ee8aa
nericarrera <nericarrera@yahoo.com.ar>
2025-08-05 21:49:16 -0300

    ok

--- my-app/src/app/page.tsx
+++ my-app/src/app/page.tsx
@@ -1,30 +1,28 @@
 import ProductCarousel from './components/ProductCarousel';
-import { getFeaturedProducts } from './data/Products';
+import { getFeaturedProducts } from './data/products'; // Usando alias @
 import VideoCarousel from './components/VideoCarousel';
-import CategorySelector from './components/CategorySelector'; // Importa el nuevo componente
+import CategorySelector from './components/CategorySelector';
 
 export default async function Home() {
-  const womenProducts = await getFeaturedProducts('mujer');
-  const menProducts = await getFeaturedProducts('hombre');
-  const kidsProducts = await getFeaturedProducts('niños');
-  const accessoriesProducts = await getFeaturedProducts('accesorios');
+  const [womenProducts, menProducts, kidsProducts, accessoriesProducts] = await Promise.all([
+    getFeaturedProducts('mujer'),
+    getFeaturedProducts('hombre'),
+    getFeaturedProducts('niños'),
+    getFeaturedProducts('accesorios')
+  ]);
 
   return (
     <div className="bg-white">
-      {/* VideoCarousel como hero banner - contenedor principal */}
       <div className="relative w-full h-[500px] overflow-hidden">
         <VideoCarousel />
       </div>
 
-      {/* Slogan */}
       <div className="py-8 text-center bg-black text-yellow-400">
         <p className="text-4xl font-light">MAKE A DIFFERENCE</p>
       </div>
 
-      {/* Nuevo selector de categorías */}
       <CategorySelector />
 
-      {/* Carruseles de productos */}
       <div className="bg-transparent mx-auto px-4">
         <ProductCarousel title="Novedades Mujer" products={womenProducts} />
         <ProductCarousel title="Novedades Hombre" products={menProducts} />
