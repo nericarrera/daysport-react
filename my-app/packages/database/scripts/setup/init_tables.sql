ad1e4d628dafa33c2467256643889e320f04e3c1
nericarrera <nericarrera@yahoo.com.ar>
2025-08-07 10:35:59 -0300

    ok

--- my-app/packages/database/scripts/setup/init_tables.sql
+++ my-app/packages/database/scripts/setup/init_tables.sql
@@ -1,11 +1,11 @@
-CREATE TABLE IF NOT EXISTS users (
+CREATE TABLE IF NOT EXISTS test_products (
     id SERIAL PRIMARY KEY,
     name VARCHAR(255) NOT NULL,
-    price DECIMAL(10, 2) NOT NULL,
-    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+    price DECIMAL(10, 2) NOT NULL
 );
 
+-- Luego inserta los datos
 INSERT INTO test_products (name, price) VALUES
-('camiseta Deportiva', 29.99),
-('Short de Runnig', 24.500),
-('Zapatillas premium', 89.99);
+    ('Camiseta Deportiva', 29.99),
+    ('Short de Running', 24.50),
+    ('Zapatillas Premium', 89.99);
