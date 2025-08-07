a5549f25f6585eae24d1514f95e19b6ad0d6ab9d
nericarrera <nericarrera@yahoo.com.ar>
2025-08-07 19:45:24 -0300

    ok

--- my-app/apps/backend/src/main.ts
+++ my-app/apps/backend/src/main.ts
@@ -1,11 +1,9 @@
+import { NestFactory } from '@nestjs/core';
+import { AppModule } from './app.module';
+
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  
-  app.enableCors({
-    origin: 'http://localhost:3000', // URL de tu frontend
-    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
-    credentials: true,
-  });
-
-  await app.listen(3001); // Usa otro puerto diferente al frontend
+  await app.listen(3001);
+  console.log(`Application is running on: ${await app.getUrl()}`);
 }
+bootstrap();
