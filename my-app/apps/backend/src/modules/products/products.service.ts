c1ddbf4fd1b35967afcda97900cd416e7727c3f0
nericarrera <nericarrera@yahoo.com.ar>
2025-08-07 22:05:16 -0300

    ok

--- my-app/apps/backend/src/modules/products/products.service.ts
+++ my-app/apps/backend/src/modules/products/products.service.ts
@@ -5,12 +5,7 @@ import { PrismaService } from '../../prisma/prisma.service';
 export class ProductsService {
   constructor(private prisma: PrismaService) {}
 
-  async findByCategory(category: string) {
-    return this.prisma.product.findMany({
-      where: { category },
-      include: {
-      images: true
-      }
-    });
+  async findAll() {
+    return this.prisma.product.findMany();
   }
 }
