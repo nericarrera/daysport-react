de371a22b858a105d3df9dd0dc4931d681550207
nericarrera <nericarrera@yahoo.com.ar>
2025-08-11 16:20:46 -0300

    ok

---
+++ my-app/apps/backend/dist/modules/products/dto/register.dto.js
@@ -0,0 +1,9 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.RegisterDto = void 0;
+class RegisterDto {
+    email;
+    password;
+    name;
+}
+exports.RegisterDto = RegisterDto;
