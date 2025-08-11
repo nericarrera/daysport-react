393fbdadafead93cca0ff08acd6579de2b34811e
nericarrera <nericarrera@yahoo.com.ar>
2025-08-11 08:36:58 -0300

    ok

--- my-app/apps/backend/src/prisma/prisma.config.ts
+++
@@ -1,7 +0,0 @@
-// prisma.config.ts
-import { defineConfig } from '@prisma/internals'
-
-export default defineConfig({
-  schema: 'apps/backend/src/prisma/schema.prisma',
-  out: 'apps/backend/src/prisma/generated'
-})
