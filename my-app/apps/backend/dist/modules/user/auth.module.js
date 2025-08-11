1be48bde412ca8b94c913f0de5c5ad63d87f9186
nericarrera <nericarrera@yahoo.com.ar>
2025-08-11 16:20:46 -0300

    ok

---
+++ my-app/apps/backend/dist/modules/user/auth.module.js
@@ -0,0 +1,51 @@
+"use strict";
+var __decorate = (this && this.__decorate) || function (decorators, target, key, desc) {
+    var c = arguments.length, r = c < 3 ? target : desc === null ? desc = Object.getOwnPropertyDescriptor(target, key) : desc, d;
+    if (typeof Reflect === "object" && typeof Reflect.decorate === "function") r = Reflect.decorate(decorators, target, key, desc);
+    else for (var i = decorators.length - 1; i >= 0; i--) if (d = decorators[i]) r = (c < 3 ? d(r) : c > 3 ? d(target, key, r) : d(target, key)) || r;
+    return c > 3 && r && Object.defineProperty(target, key, r), r;
+};
+var __metadata = (this && this.__metadata) || function (k, v) {
+    if (typeof Reflect === "object" && typeof Reflect.metadata === "function") return Reflect.metadata(k, v);
+};
+var __param = (this && this.__param) || function (paramIndex, decorator) {
+    return function (target, key) { decorator(target, key, paramIndex); }
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.AuthController = void 0;
+// apps/backend/src/auth/auth.controller.ts
+const common_1 = require("@nestjs/common");
+const prisma_service_1 = require("../../../prisma/prisma.service");
+let AuthController = class AuthController {
+    prisma;
+    constructor(prisma) {
+        this.prisma = prisma;
+    }
+    async register(data) {
+        try {
+            const user = await this.prisma.user.create({
+                data: {
+                    email: data.email,
+                    password: data.password, // En producción usa bcrypt!
+                    name: data.name,
+                },
+            });
+            return { success: true, user };
+        }
+        catch (error) {
+            return { success: false, error: 'El email ya existe' };
+        }
+    }
+};
+exports.AuthController = AuthController;
+__decorate([
+    (0, common_1.Post)('register'),
+    __param(0, (0, common_1.Body)()),
+    __metadata("design:type", Function),
+    __metadata("design:paramtypes", [Object]),
+    __metadata("design:returntype", Promise)
+], AuthController.prototype, "register", null);
+exports.AuthController = AuthController = __decorate([
+    (0, common_1.Controller)('auth'),
+    __metadata("design:paramtypes", [prisma_service_1.PrismaService])
+], AuthController);
