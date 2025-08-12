5fa64f13ab1cc326a3b8abd75552d51b78fe298b
nericarrera <nericarrera@yahoo.com.ar>
2025-08-12 10:59:03 -0300

    ok

--- my-app/apps/backend/src/modules/auth/auth.module.ts
+++ my-app/apps/backend/src/modules/auth/auth.module.ts
@@ -5,7 +5,7 @@ import { PassportModule } from '@nestjs/passport';
 import { AuthService } from './auth.service';
 import { AuthController } from './auth.controller';
 import { PrismaService } from '../../../prisma/prisma.service';
-import { JwtStrategy } from 'jwt.strategy';
+import { JwtStrategy } from './jwt.strategy';
 
 @Module({
   imports: [
