ef965c488456650dbd59043778d0d8472a699320
nericarrera <nericarrera@yahoo.com.ar>
2025-08-13 19:07:05 -0300

    ok

--- my-app/apps/backend/dist/src/main.js
+++ my-app/apps/backend/dist/src/main.js
@@ -2,13 +2,21 @@
 Object.defineProperty(exports, "__esModule", { value: true });
 const core_1 = require("@nestjs/core");
 const app_module_1 = require("./app.module");
+const common_1 = require("@nestjs/common"); // <-- Importar aquí
 async function bootstrap() {
     const app = await core_1.NestFactory.create(app_module_1.AppModule);
+    // Habilitar CORS (temporal para pruebas)
     app.enableCors({
-        origin: "*", // temporal para pruebas; luego podés restringir a tu frontend
+        origin: "*",
         methods: "GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS",
     });
-    await app.listen(3001, '0.0.0.0'); // <- muy importante
+    // Activar validación global de DTOs
+    app.useGlobalPipes(new common_1.ValidationPipe({
+        whitelist: true, // elimina propiedades que no estén en el DTO
+        forbidNonWhitelisted: true, // lanza error si llega algo extra
+        transform: true, // convierte automáticamente tipos (por ejemplo string -> number)
+    }));
+    await app.listen(3001, '0.0.0.0');
     console.log(`Backend corriendo en http://0.0.0.0:3001`);
 }
 bootstrap();
