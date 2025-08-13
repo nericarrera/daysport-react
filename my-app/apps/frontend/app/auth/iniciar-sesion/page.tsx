218ffc29e2735db5829aca0d42f53aba3fa30a62
nericarrera <nericarrera@yahoo.com.ar>
2025-08-13 14:54:14 -0300

    ok

--- my-app/apps/frontend/app/auth/iniciar-sesion/page.tsx
+++ my-app/apps/frontend/app/auth/iniciar-sesion/page.tsx
@@ -14,7 +14,7 @@ export default function IniciarSesionPage() {
     setMensaje('');
 
     try {
-      const res = await fetch('http://localhost:3001/auth/login', {
+      const res = await fetch('http://localhost:3001/auth/iniciar-sesion', {
         method: 'POST',
         headers: { 'Content-Type': 'application/json' },
         body: JSON.stringify({ email, password }),
--- my-app/apps/frontend/app/auth/iniciar-sesion/registrarse/page.tsx
+++ my-app/apps/frontend/app/auth/iniciar-sesion/registrarse/page.tsx
@@ -14,7 +14,7 @@ export default function RegistrarsePage() {
     setMensaje('');
 
     try {
-      const res = await fetch('http://localhost:3001/auth/register', {
+      const res = await fetch('http://localhost:3001/auth/registrarse', {
         method: 'POST',
         headers: { 'Content-Type': 'application/json' },
         body: JSON.stringify({ email, password }),
