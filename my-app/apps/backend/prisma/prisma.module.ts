588501577bfdcbcc5a34729f19e4e090046f909d
nericarrera <nericarrera@yahoo.com.ar>
2025-08-14 08:44:31 -0300

    ok

--- my-app/apps/backend/prisma/prisma.module.ts
+++ my-app/apps/backend/prisma/prisma.module.ts
@@ -3,6 +3,6 @@ import { PrismaService } from './prisma.service';
 
 @Module({
   providers: [PrismaService],
-  exports: [PrismaService],
+  exports: [PrismaService], // 🔑 Muy importante exportarlo para otros módulos
 })
 export class PrismaModule {}
