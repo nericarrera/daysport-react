95f4b35439799729e9b6b0ddcbb3adb5e389647a
nericarrera <nericarrera@yahoo.com.ar>
2025-08-21 22:43:55 -0300

    ok

--- my-app/apps/frontend/.next/types/app/accesorios/page.ts
+++ my-app/apps/frontend/.next/types/app/accesorios/page.ts
@@ -1,8 +1,8 @@
-// File: C:\Users\nerip\Documents\daysport-react\my-app\apps\frontend\app\hombre\page.tsx
-import * as entry from '../../../../app/hombre/page.js'
+// File: C:\Users\nerip\Documents\daysport-react\my-app\apps\frontend\app\accesorios\page.tsx
+import * as entry from '../../../../app/accesorios/page.js'
 import type { ResolvingMetadata, ResolvingViewport } from 'next/dist/lib/metadata/types/metadata-interface.js'
 
-type TEntry = typeof import('../../../../app/hombre/page.js')
+type TEntry = typeof import('../../../../app/accesorios/page.js')
 
 type SegmentParams<T extends Object = any> = T extends Record<string, any>
   ? { [K in keyof T]: T[K] extends string ? string | string[] | undefined : never }
