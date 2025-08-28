555257b084c8d3658e8dd9c4df2061e6ba0ae097
nericarrera <nericarrera@yahoo.com.ar>
2025-08-28 16:47:31 -0300

    ok

--- my-app/next.config.ts
+++ my-app/next.config.ts
@@ -26,7 +26,7 @@ const nextConfig = {
       },
       {
         protocol: 'https',
-        hostname: '**.unsplash.com', // Para todos los subdominios
+        hostname: '.unsplash.com', // Para todos los subdominios
       },
     ],
   },
