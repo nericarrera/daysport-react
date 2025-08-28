05357b405cfd34d8064b224034e2ece2fd9cc46c
nericarrera <nericarrera@yahoo.com.ar>
2025-08-28 11:51:41 -0300

    ok

--- my-app/apps/backend/src/modules/products/products.service.ts
+++ my-app/apps/backend/src/modules/products/products.service.ts
@@ -1,6 +1,6 @@
 import { Injectable } from '@nestjs/common';
 import { PrismaService } from '../../../prisma/prisma.service';
-import { Prisma, Product } from '@prisma/client';
+import { Prisma, Product } from '.prisma/client';
 
 @Injectable()
 export class ProductsService {
