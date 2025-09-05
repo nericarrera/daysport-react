0b0c8ec91d0c116e8184503313da6fc6cc9c2132
nericarrera <nericarrera@yahoo.com.ar>
2025-09-05 01:01:27 -0300

    ok

--- my-app/apps/backend/src/modules/cart/cart.service.ts
+++ my-app/apps/backend/src/modules/cart/cart.service.ts
@@ -5,29 +5,29 @@ import { PrismaService } from '../../../prisma/prisma.service';
 export class CartService {
   constructor(private prisma: PrismaService) {}
 
-  async addItem(userId: number, productId: number, quantity: number = 1) {
+  async addItem(userId: String, productId: String, quantity: number = 1) {
     const cart = await this.getOrCreateCart(userId);
     
     return this.prisma.cartItem.upsert({
       where: {
         cartId_productId: {
           cartId: cart.id,
-          productId
+          productId: productId.toString(),
         }
       },
       update: { quantity },
       create: {
         cartId: cart.id,
-        productId,
+        productId: productId.toString(),
         quantity
       }
     });
   }
 
-  private async getOrCreateCart(userId: number) {
+  private async getOrCreateCart(userId: String) {
     return this.prisma.cart.upsert({
-      where: { userId },
-      create: { userId },
+      where: { userId: userId.toString() },
+      create: { userId: userId.toString() },
       update: {}
     });
   }
