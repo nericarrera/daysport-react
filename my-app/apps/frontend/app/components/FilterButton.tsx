b985a4672a7079f5b09501f8c248414f3048a59c
nericarrera <nericarrera@yahoo.com.ar>
2025-09-05 13:46:00 -0300

    ok

--- my-app/apps/frontend/app/components/FilterButton.tsx
+++ my-app/apps/frontend/app/components/FilterButton.tsx
@@ -1,6 +1,22 @@
 'use client';
 import { useState, useEffect, useRef} from 'react';
 
+export function useDebounce<T>(value: T, delay: number): T {
+  const [debouncedValue, setDebouncedValue] = useState<T>(value);
+
+  useEffect(() => {
+    const handler = setTimeout(() => {
+      setDebouncedValue(value);
+    }, delay);
+
+    return () => {
+      clearTimeout(handler);
+    };
+  }, [value, delay]);
+
+  return debouncedValue;
+}
+
 interface FilterButtonProps {
   category: string;
   onFilterChange?: (subcategory: string) => void;
