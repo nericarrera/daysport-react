d6833452ad3f17c60d73afbe6fcafb22d91a977a
nericarrera <nericarrera@yahoo.com.ar>
2025-09-04 23:33:39 -0300

    ok

---
+++ my-app/apps/backend/prisma/migrations/20250905023331_change_ids_to_string/migration.sql
@@ -0,0 +1,74 @@
+/*
+  Warnings:
+
+  - The primary key for the `Cart` table will be changed. If it partially fails, the table could be left without primary key constraint.
+  - The primary key for the `CartItem` table will be changed. If it partially fails, the table could be left without primary key constraint.
+  - The primary key for the `Product` table will be changed. If it partially fails, the table could be left without primary key constraint.
+  - The primary key for the `SizeGuide` table will be changed. If it partially fails, the table could be left without primary key constraint.
+  - The primary key for the `User` table will be changed. If it partially fails, the table could be left without primary key constraint.
+
+*/
+-- DropForeignKey
+ALTER TABLE "public"."Cart" DROP CONSTRAINT "Cart_userId_fkey";
+
+-- DropForeignKey
+ALTER TABLE "public"."CartItem" DROP CONSTRAINT "CartItem_cartId_fkey";
+
+-- DropForeignKey
+ALTER TABLE "public"."CartItem" DROP CONSTRAINT "CartItem_productId_fkey";
+
+-- DropForeignKey
+ALTER TABLE "public"."ResetToken" DROP CONSTRAINT "ResetToken_userId_fkey";
+
+-- AlterTable
+ALTER TABLE "public"."Cart" DROP CONSTRAINT "Cart_pkey",
+ALTER COLUMN "id" DROP DEFAULT,
+ALTER COLUMN "id" SET DATA TYPE TEXT,
+ALTER COLUMN "userId" SET DATA TYPE TEXT,
+ADD CONSTRAINT "Cart_pkey" PRIMARY KEY ("id");
+DROP SEQUENCE "Cart_id_seq";
+
+-- AlterTable
+ALTER TABLE "public"."CartItem" DROP CONSTRAINT "CartItem_pkey",
+ALTER COLUMN "id" DROP DEFAULT,
+ALTER COLUMN "id" SET DATA TYPE TEXT,
+ALTER COLUMN "cartId" SET DATA TYPE TEXT,
+ALTER COLUMN "productId" SET DATA TYPE TEXT,
+ADD CONSTRAINT "CartItem_pkey" PRIMARY KEY ("id");
+DROP SEQUENCE "CartItem_id_seq";
+
+-- AlterTable
+ALTER TABLE "public"."Product" DROP CONSTRAINT "Product_pkey",
+ALTER COLUMN "id" DROP DEFAULT,
+ALTER COLUMN "id" SET DATA TYPE TEXT,
+ADD CONSTRAINT "Product_pkey" PRIMARY KEY ("id");
+DROP SEQUENCE "Product_id_seq";
+
+-- AlterTable
+ALTER TABLE "public"."ResetToken" ALTER COLUMN "userId" SET DATA TYPE TEXT;
+
+-- AlterTable
+ALTER TABLE "public"."SizeGuide" DROP CONSTRAINT "SizeGuide_pkey",
+ALTER COLUMN "id" DROP DEFAULT,
+ALTER COLUMN "id" SET DATA TYPE TEXT,
+ADD CONSTRAINT "SizeGuide_pkey" PRIMARY KEY ("id");
+DROP SEQUENCE "SizeGuide_id_seq";
+
+-- AlterTable
+ALTER TABLE "public"."User" DROP CONSTRAINT "User_pkey",
+ALTER COLUMN "id" DROP DEFAULT,
+ALTER COLUMN "id" SET DATA TYPE TEXT,
+ADD CONSTRAINT "User_pkey" PRIMARY KEY ("id");
+DROP SEQUENCE "User_id_seq";
+
+-- AddForeignKey
+ALTER TABLE "public"."CartItem" ADD CONSTRAINT "CartItem_cartId_fkey" FOREIGN KEY ("cartId") REFERENCES "public"."Cart"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "public"."CartItem" ADD CONSTRAINT "CartItem_productId_fkey" FOREIGN KEY ("productId") REFERENCES "public"."Product"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "public"."Cart" ADD CONSTRAINT "Cart_userId_fkey" FOREIGN KEY ("userId") REFERENCES "public"."User"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "public"."ResetToken" ADD CONSTRAINT "ResetToken_userId_fkey" FOREIGN KEY ("userId") REFERENCES "public"."User"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
